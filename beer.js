7c220b20ffcba933752d5364bb49f08f4640876e
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2018-04-23 18:57:55 -0600

    changes made

--- beer.js
+++ beer.js
@@ -1,22 +1,31 @@
 // pull concurrent list from server
-function getDataFromServer() {
-
-}
+function getDataFromServer() {}
 //render list
 function renderBeerList() {
-
+	const results = data.BEERMENU.map((items, index) => displayBeers(item);)
 }
 //display list to user
 function displayBeers() {
-
+	return `
+	<div class="beer-select">
+	<label id="beer-id" for="thumbnail">${BEERMENU[i].name}</label>
+	<img src="${BEERMENU[i].img}" alt="${BEERMENU[i].alt}" name="thumbnail">
+	<p class="beer-text">Brewery: ${BEERMENU[i].brewery}<span class="locale">(${BEERMENU[i].location})</span></p>
+	<p class="beer-text">ABV: ${BEERMENU[i].ABV}</p>
+	<p class="beer-text">IBU Rating: ${BEERMENU[i].bitterness}</p>
+	<p class="beer-text">Best paired with: ${BEERMENU[i].pairing}</p>
+	</div> 
+	`
 }
 //listen for add
 function listenForEvent() {
-
+	$('.nav').on('click', '#add-beer', function(event) {
+		selectNewOnTap();
+	});
 }
 //if add, produce dropdown menu with beers already registered
 function selectNewOnTap() {
-
+	//toggle hidden class on beer list
 }
 //if new beer selected, redirect to new beer form.html
 function listenForNewBeer() {
